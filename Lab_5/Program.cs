6f3207ae831b3c876249c9f9da9155d321a4c687
avolodin1479@gmail.com <avolodin1479@gmail.com>
2021-10-08 21:18:34 +0600

    Добавьте файлы проекта.

---
+++ Lab_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] a = new double[7];
+            double sum = 0;
+
+            Console.WriteLine("Введите 7 элементов массива ");
+            for (int i = 0; i < 7; i++)
+            {
+                a[i] = Convert.ToDouble(Console.ReadLine());
+                sum += a[i];
+            }
+
+            double aM = sum / 7;
+            Console.WriteLine("Среднее арифметическое равно {0}", aM);
+            Console.ReadKey();
+            }
+    }
+}
